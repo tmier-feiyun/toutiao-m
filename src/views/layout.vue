2438d09356139decc6d827069c38451b3a7a490d
Tmier <1042970366@qq.com>
2019-11-28 21:27:16 +0800

    项目初始化

---
+++ src/views/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <!-- 导航栏 -->
+      <!-- 内容容器 -->
+      <!-- tab栏 -->
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+</style>
